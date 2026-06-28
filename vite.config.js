d03f9c431d98c82d4e3c5052479b8d504e5fb4a7
Mark <user@users-MacBook-Air.local>
2026-06-28 16:57:18 +0800

    Update site

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                member: "member.html",
+            },
+        },
+    },
 });
